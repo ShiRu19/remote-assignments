bc66b1b82b2de1eacd2b3380344f1c68bcc1403c
ShiRu19 <afa890419@gmail.com>
2023-06-07 20:04:50 +0800

    Assignment2

---
+++ Week-3/Assignment-1/Week3_Assignment_1/Week3_Assignment_1/Controllers/DataController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+
+namespace Week3_Assignment_1.Controllers
+{
+    public class DataController : Controller
+    {
+        public string Index(string? number)
+        {
+            string message = "";
+
+            if(number == null)
+            {
+                message = "Lack of Parameter";
+            }
+            else
+            {
+                BigInteger num;
+                bool isNum = BigInteger.TryParse(number, out num);
+
+                if(isNum)
+                {
+                    BigInteger result = (1 + num) * num / 2;
+                    message = result.ToString();
+                }
+                else
+                {
+                    message = "Wrong Parameter";
+                }
+            }
+
+            return message;
+        }
+    }
+}
