3b1d19f68f5372baf832eb7151e8ee0c7ac22aea
ShiRu19 <afa890419@gmail.com>
2023-06-17 14:37:11 +0800

    Week4-Assignment-1 initial ASP.Net

---
+++ Week-4/Assignment-1/Assignment_1/Assignment_1/Data/MvcDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Assignment_1.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment_1.Data
+{
+	public class MvcDbContext : DbContext
+	{
+		public MvcDbContext(DbContextOptions options) : base(options)
+		{
+		}
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Week-4/Assignment-1/Assignment_1/Assignment_1/Models/Domain/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment_1.Models.Domain
+{
+	public class User
+	{
+		public Guid Id { get; set; }
+		public string Email { get; set; }
+		public string password { get; set; }
+	}
+}
